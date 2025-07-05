cc59d1591451e3a38700a9585ed8862d9817ca9d
infinity <egold1920@gmail.com>
2025-07-05 21:35:58 +0700

    add psr-4 autoload

--- src/include/bootstrap.php
+++ src/include/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
-require_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../crest/crest.php';
 include_once __DIR__ . '/../functions.php';
